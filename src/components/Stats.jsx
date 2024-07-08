eb89baead3319873f02836f21873d144b861cdce
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-07-08 17:46:34 +0200

    add stats cards

---
+++ src/components/Stats.jsx
@@ -0,0 +1,23 @@
+import './stats.css'
+export default function Stats() {
+    return (
+        <div className="container stats">
+            <div className="card">
+                <h3>100+</h3>
+                <h4>MAGHI DEL CODICE</h4>
+            </div>
+            <div className="card">
+                <h3>500+</h3>
+                <h4>CORSI STRAVAGANTI</h4>
+            </div>
+            <div className="card">
+                <h3>GRATIS</h3>
+                <h4>UNA PACCA SULLA SPALLA</h4>
+            </div>
+            <div className="card">
+                <h3>2,000+</h3>
+                <h4>PERSONE CI SONO CASCATE</h4>
+            </div>
+        </div>
+    )
+}
