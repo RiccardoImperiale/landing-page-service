123c769b837c3b7acf7133e4ae4668e4d6e11cfb
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-07-08 14:47:34 +0200

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="banner">Ottieni il tuo primo corso con uno sconto del 50%. L'offerta è valida solo per i primi 50 studenti. <a href="#">Registrati adesso</a></div>
+            <nav>navbar</nav>
+        </header>
+    )
+}
