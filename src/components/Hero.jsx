6271311305e33d36b3674944c32c178954f5f4ba
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-07-08 17:28:42 +0200

    add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import './hero.css'
+import { LuSearch } from "react-icons/lu";
+
+export default function Hero() {
+    return (
+        <section className="hero">
+            <div className="container">
+                <div className="hero_text">
+                    <h1>CODECIRCUS</h1>
+                    <p>Immergiti nel divertimento del codice, dove ogni byte è un gioia!</p>
+                    <div className="input_group">
+                        <input type="text" placeholder='cosa vuoi imparare oggi?' />
+                        <button><LuSearch /></button>
+                    </div>
+                </div>
+                <div className="hero_image">
+                    <img src="/img/billboard-img.png" alt="image of a clown seated working on the computer" />
+                </div>
+            </div>
+        </section>
+    )
+}
